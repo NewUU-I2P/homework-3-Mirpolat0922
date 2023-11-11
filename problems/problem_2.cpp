0bb918975c96740cef3a69d50f126d296c01d7b2
mirpolat <m.erkinov@newuu.uz>
2023-11-11 22:17:14 +0500

    Problem 2

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,9 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+    double price1, price2, price3;
+    cin >> price1>>price2>>price3;
+    double highestPrice = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
 
     return result;
 }
--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,28 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+    string sex,result;
+    double length;
+    cin>>sex<<length;
+    switch(sex){
+        case "Male":
+            if(length<1.7){
+                result = "Short";
+            }else if(length>= 1.7 and length <1.85){
+                result = "Normal";
+            }else{
+                result = "Tall"
+            }
+        case "Female":
+            if(length<1.6){
+                result = "Short";
+            }else if(length>= 1.6 and length <1.75){
+                result = "Normal";
+            }else{
+                result = "Tall"
+            }
+    }
+    return result;
     // use return to return your result
     // make use of control flow statements
 }
