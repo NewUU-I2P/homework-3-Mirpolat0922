05974f3a4a561561d6473f4c9b04f2b3ba1b9a5f
mirpolat <m.erkinov@newuu.uz>
2023-11-11 21:38:13 +0500

    Problem 1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,16 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-
+    double a;
+    cin>>a;
+    if(a>=60){
+        cout<<13+30*0.4+20*0.12+10*1.4+(a-60)*1.5;
+    }else if(a>=50 and a<60){
+        cout<<13+30*0.4+20*0.12+(a-50)*1.4;
+    }else if(a>=30 and a<50) {
+        cout << 13 + 30 * 0.4 + (a-30) * 0.12;
+    }else if(a>=0 and a<30) {
+        cout << 13 + a * 0.4;
+    }
     return cost;
 }
