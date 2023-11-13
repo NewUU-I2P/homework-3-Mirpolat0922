cdf2ba7986f6d38bbfb22725d1a5ea2242b979e2
mirpolat <m.erkinov@newuu.uz>
2023-11-13 13:32:29 +0500

    Problem 2

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,7 @@
 float problemSolution2(float i, float j, float k) {
-    int result;
+    double result;
     // write your code here;
-    std::cin >> i>>j>>k;
+    cin >>i>>j>>k;
     result = (i > j) ? ((i > k) ? i : k) : ((j > k) ? j : k);
     return result;
 }
